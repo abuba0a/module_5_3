ca8810296ba19714cbbf9e11d5b6da012c709166
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-06-18 14:36:02 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,1 +1,13 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors , self.buildingType == other.buildingType
+
+
+Building1 = Building(10, 'Жилой дом')
+Building2 = Building(10, 'Офисное здание')
+
+print(Building1 == Building2)
